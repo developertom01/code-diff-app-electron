c097b8805e4be04b97282a3ebfd0e02777153225
Thomas <thomas.sarpong@yum.com>
2023-12-10 00:51:30 -0500

    Added Seperated ipc handler interface

--- electron/ipc/index.ts
+++ electron/ipc/index.ts
@@ -1,17 +1,6 @@
-import { BrowserWindow, dialog, ipcMain } from "electron"
+import { MessagesAndDialog } from "./messagesAndDialogue"
 
-enum IPC_CHANNELS { 
-    SHOW_DIALOGUE = "SHOW_DIALOGUE",
-    SHOW_ERROR_DIALOGUE = "SHOW_ERROR_DIALOGUE"
-}
-
-type IPCErrorMessage = {
-    message: string
-} 
 
 export const createEventHandlers= ()=>{
-    ipcMain.on(IPC_CHANNELS.SHOW_ERROR_DIALOGUE,(ev, args:IPCErrorMessage)=>{
-        const win = BrowserWindow.fromWebContents(ev.sender)!
-        dialog.showMessageBox(win, {message: args.message})
-    })
+    MessagesAndDialog.listen()
 }
--- electron/ipc/messagesAndDialogue.ts
+++ electron/ipc/messagesAndDialogue.ts
@@ -8,10 +8,15 @@ enum IPC_CHANNELS {
 type IPCErrorMessage = {
     message: string
 } 
+export class MessagesAndDialog{
+    private static showErrorDialogue(){
+        ipcMain.on(IPC_CHANNELS.SHOW_ERROR_DIALOGUE,(ev, args:IPCErrorMessage)=>{
+            const win = BrowserWindow.fromWebContents(ev.sender)!
+            dialog.showMessageBox(win, {message: args.message})
+        })
+    }
 
-export const createEventHandlers= ()=>{
-    ipcMain.on(IPC_CHANNELS.SHOW_ERROR_DIALOGUE,(ev, args:IPCErrorMessage)=>{
-        const win = BrowserWindow.fromWebContents(ev.sender)!
-        dialog.showMessageBox(win, {message: args.message})
-    })
+    public static listen(){
+        this.showErrorDialogue()
+    }
 }
